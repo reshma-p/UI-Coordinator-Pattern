2db47f20c9f478a02db68f13bec9ab461925af6f
Reshma Pinto <reshma.pinto@igt.com>
2019-09-10 23:58:20 +0100

    added project and Coordinator protocol

---
+++ CoordinatorPattern/CoordinatorPattern/Coordinator.swift
@@ -0,0 +1,20 @@
+//
+//  Coordinator.swift
+//  CoordinatorPattern
+//
+//  Created by Reshma Pinto on 10/09/2019.
+//  Copyright © 2019 RP. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// This protocol defines the basic contract for a Coordinator
+protocol Coordinator{
+    
+    var childCoordinators :[Coordinator] { get set}
+    
+    var navigationController : UINavigationController
+    
+    func start() -> Void
+}
---
+++ CoordinatorPattern/CoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Reshma Pinto on 10/09/2019.
+//  Copyright © 2019 RP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
